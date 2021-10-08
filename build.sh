c1f445ec4cc167c677677a42aa094df6debb94fc
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-10-08 14:45:53 +0530

    Update build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# dockerhub username:
+REGISTRY=shubhamtatvamasi
+
+# magma variables:
+MAGMA_ROOT=${PWD}/magma
+PUBLISH=${MAGMA_ROOT}/orc8r/tools/docker/publish.sh
+
+# Cloning magma repo:
+git clone -b agw-containerization https://github.com/arunuke/magma.git --depth 1
+
+cd ${MAGMA_ROOT}
+MAGMA_TAG=$(git rev-parse --short HEAD)
+
+# Deleting docker login code block:
+sed -i '65,71d' ${PUBLISH}
+
+# Building Access Gateway docker images:
+cd ${MAGMA_ROOT}/lte/gateway/docker
+docker-compose build
+
+docker images
+
